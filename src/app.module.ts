f0de23e00fe834c02de167c402313df97c3b97cb
Fabiana <fabiana.asara@gmail.com>
2021-11-01 17:04:31 +0000

    wip Adds connection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'rotacloud',
+      database: 'test',
+
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
